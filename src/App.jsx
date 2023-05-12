70dd6b639ebb955ca7de308597a9433adc0e1e9f
MauvG <muaviaijazghazi@gmail.com>
2023-05-12 19:49:36 +0100

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Announcement from "./components/Announcement";
 import Navbar from "./components/Navbar";
+import Slider from "./components/Slider";
 import Homepage from "./pages/Homepage";
 
 const App = () => {
   return (
-    <Navbar /> 
+    <div>
+      <Announcement />
+      <Navbar />
+      <Slider />
+    </div> 
   );
 };
 
