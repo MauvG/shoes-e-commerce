9d2ac4ab2b55be783783afd182cf1f1cab9ede23
MauvG <muaviaijazghazi@gmail.com>
2023-05-12 20:41:29 +0100

    Added featured shoes

---
+++ src/sliderData.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        image: "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/59ea4d4c-6472-4673-82de-70b698a5075c/dunk-low-shoes-VB5h2T.png",
+        title: "Nike Dunk Low",
+        description: "Created for the hardwood but taken to the streets, this '80s b-ball icon returns with classic details and throwback hoops flair. Supple suede overlays help the Nike Dunk channel vintage style while its padded, low-cut collar lets you take your game anywhere—in comfort. Subtle hints of colour nod to the 40th anniversary of the Pegasus, so you can celebrate sneaker legacy with each step.", 
+        background: "",
+    },
+    {
+        id: 2,
+        image: "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/9882f76dc5b14339a92bac5a0160ac4f_9366/Samba_Vegan_Shoes_White_H01877_01_standard.jpg",
+        title: "SAMBA VEGAN SHOES",
+        description: "First introduced as an indoor football shoe, the adidas Samba has gone on to cover expansive ground. After their time on the field, these shoes made their mark on stages and streets across the globe. Today they look toward a more conscious future. Done up in faux leather, the revamped icon has eliminated any usage of animal based materials. The look is exactly the same as the original, right down to the famous T-toe.", 
+        background: "",
+    },
+    {
+        id: 3,
+        image: "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_1350,h_1350/global/391925/01/sv01/fnd/EEA/fmt/png/Rider-FV-Pop-FS-Sneakers",
+        title: "Rider FV Pop FS Sneakers",
+        description: "The newest addition to the Rider family is celebrating the past while standing firmly in the present – just like the people who wear it. Rider FV is all about a combination of past and future, with a nostalgic, heritage-inspired upper and fresh tooling. This execution features a nylon upper with suede overlays.", 
+        background: "",
+    },
+]
